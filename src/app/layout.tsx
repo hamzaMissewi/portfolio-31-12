72908950f7602fccf78ff742ee45fdd55e23dd39
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-04 12:12:33 +0100

    commit before deploy to netlify of portfolio with next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
+import "./globals.css";
 // import type { Metadata } from "next";
 // export const metadata: Metadata = {
 //   title: "Portfolio Next Hamza",
 //   description: "Resume about me as web developer react - next - typescript",
 // };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  return <main>{children}</main>;
+  return (
+    // <Document>
+    <main>{children}</main>
+  );
 }
