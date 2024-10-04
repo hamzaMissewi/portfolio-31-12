48d238b96e1f9058f5ea6c0dc6f6edfc98bf223d
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-04 15:07:19 +0100

    🚀before deploy to netlify of portfolio with next js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import "./globals.css";
+// import "./globals.css";
+
 // import type { Metadata } from "next";
+
 // export const metadata: Metadata = {
 //   title: "Portfolio Next Hamza",
 //   description: "Resume about me as web developer react - next - typescript",
